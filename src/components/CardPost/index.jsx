0d6d0dbe739825955c23193e68b38b98421e415e
braiancalpar <braian.santos@calpar.com.br>
2026-06-09 15:09:24 -0300

    Comentários restritos apenas para usuários

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -6,24 +6,34 @@ import styles from "./cardpost.module.css";
 import { ThumbsUpButton } from "./ThumbsUpButton";
 import { Link } from "react-router";
 import { http } from "../../api";
+import { useAuth } from "../../hooks/useAuth";
 
 export const CardPost = ({ post }) => {
   const [likes, setLikes] = useState(post.likes);
+  const [comments, setComments] = useState(post.comments);
+  const { isAuthenticated } = useAuth();
+
+  const handleNewComment = (comment) => {
+    setComments([comment, ...comments]);
+  };
 
   const handleLikeButton = () => {
+    const token = localStorage.getItem("access_token");
 
-    const token = localStorage.getItem('access_token')
-
-    http.post(`blog-posts/${post.id}/like`, {}, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-    .then(() => {
-          setLikes((oldState) => oldState + 1);
-          console.log("incrementar like");
-      },
-    );
+    http
+      .post(
+        `blog-posts/${post.id}/like`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+      .then(() => {
+        setLikes((oldState) => oldState + 1);
+        console.log("incrementar like");
+      });
   };
 
   return (
@@ -41,12 +51,16 @@ export const CardPost = ({ post }) => {
       <footer className={styles.footer}>
         <div className={styles.actions}>
           <div className={styles.action}>
-            <ThumbsUpButton loading={false} onClick={handleLikeButton} />
+            <ThumbsUpButton
+              loading={false}
+              onClick={handleLikeButton}
+              disabled={!isAuthenticated}
+            />
             <p>{likes}</p>
           </div>
           <div className={styles.action}>
-            <ModalComment />
-            <p>{post.comments.length}</p>
+            <ModalComment onSuccess={handleNewComment} postId={post.id} />
+            <p>{comments.length}</p>
           </div>
         </div>
         <Author author={post.author} />
