1bc441eec7bdddf0c91e19e81d1823c3dada5e50
braiancalpar <braian.santos@calpar.com.br>
2026-06-09 16:34:04 -0300

    Adicionado exclusão de comentários

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,25 +1,37 @@
-import styles from './comment.module.css'
-import { Avatar } from "../Avatar"
-import { ModalComment } from '../ModalComment'
-import { useAuth } from '../../hooks/useAuth'
-import { useState } from 'react'
+import styles from "./comment.module.css";
+import { Avatar } from "../Avatar";
+import { ModalComment } from "../ModalComment";
+import { useAuth } from "../../hooks/useAuth";
+import { useState } from "react";
+import { IconButton } from "../IconButton";
 
-export const Comment = ({ comment }) => {
+export const Comment = ({ comment, onDelete }) => {
+  const [text, setText] = useState(comment.text);
+  const { user } = useAuth();
 
-    const [text, setText] = useState(comment.text)
-    const { user } = useAuth()
+  const isOwner = user && user.id == comment.author.id;
 
-    const isOwner = user && user.id == comment.author.id
+  const handleEdit = (newComment) => {
+    setText(newComment.text);
+  };
 
-    const handleEdit = (newComment) => {
-        setText(newComment.text)
-    }
-
-    return (<div className={styles.comment}>
-        <Avatar author={comment.author} />
-        <strong>@{comment.author.name}</strong>
-        <p>{text}</p>
-        <div className={styles.divider} />
-        {isOwner && <ModalComment isEditing onSuccess={handleEdit} defaultValue={text} commentId={comment.id}/>}
-    </div>)
-}
+  return (
+    <div className={styles.comment}>
+      <Avatar author={comment.author} />
+      <strong>@{comment.author.name}</strong>
+      <p>{text}</p>
+      <div className={styles.divider} />
+      {isOwner && (
+        <ModalComment
+          isEditing
+          onSuccess={handleEdit}
+          defaultValue={text}
+          commentId={comment.id}
+        />
+      )}
+      {isOwner && (
+        <IconButton onClick={() => onDelete(comment.id)}>Excluir</IconButton>
+      )}
+    </div>
+  );
+};
